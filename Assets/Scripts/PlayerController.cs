fb2e935933562d45119ccaeddb29ad3fecf3bc02
Escarleto <lucascliberato31@gmail.com>
2026-03-29 18:09:18 +0100

    Read keyboard input to control 2D character movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [SerializeField] private readonly float MoveSpd = 5.0f;
+
     private void Start()
     {
         
@@ -11,6 +16,19 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        
+        float HorInput = 0.0f;
+        if (Keyboard.current.aKey.isPressed)
+            HorInput = -1.0f;
+        else if (Keyboard.current.dKey.isPressed)
+            HorInput = 1.0f;
+
+        float VerInput = 0.0f;
+        if (Keyboard.current.wKey.isPressed)
+            VerInput = 1.0f;
+        else if (Keyboard.current.sKey.isPressed)
+            VerInput = -1.0f;
+
+        transform.position += new Vector3(HorInput, VerInput, 0f) * Time.deltaTime * MoveSpd;
+
     }
 }
